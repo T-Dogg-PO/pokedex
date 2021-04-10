263f47b05e0e3bd8d44ccc745ef2bc3838a00766
T-Dogg <47765355+T-Dogg-PO@users.noreply.github.com>
2021-04-10 17:26:15 +0200

    Added pokemonList with some sample data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello world!');
+// Create a variable which will store a list of Pokemon (with corresponding data about that Pokemon)
+pokemonList = [];
 
-let favouriteFood = 'Ice Cream';
-document.write(favouriteFood);
+// Add sample data to the array to test basic functionality
+pokemonList[0] = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['Grass', 'Poison']
+}
+
+pokemonList[1] = {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['Fire']
+}
+
+pokemonList[2] = {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['Water']
+}
